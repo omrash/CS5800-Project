23408b534dcff7666d9ed81806922e25f2c3f8f7
Omar <rashwan.o@northeastern.edu>
2022-08-12 19:46:30 -0400

    Graph visualizer partly done, visualized MST pre-algorithm

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -10,12 +10,43 @@ Just import the visualize() function from this file to use.
 '''
 
 import networkx as nx
+import matplotlib.pyplot as plt
 
-def visualize(graph, array):
+def visualize(graph, array, x_offset=0, y_offset=0, scale=1):
+    '''
+    x_offset: shifts all points along the x axis
+    y_offset: shifts all points along the y axis
+    scale: scales the distance from the origin (0,0) by input
+    '''
+
+    n = len(graph)
     G = nx.Graph()
+    pos = {}
+    name = {}
+
+    for u in range(n):
+        # calculate position
+        x = (array[u][2] * scale) + x_offset
+        y = (array[u][1] * scale) + y_offset
+
+        # populate dicts
+        pos[u] = x,y
+        name[u] = array[u][0]
+        
+        # adds nodes with array data as attributes
+        G.add_node(u, pos=(x,y), name=graph[u][0], lat=graph[u][1], long=graph[u][2])
+        
+
+    for u in range(n):
+        for v in range(u+1, n):
+            # add edges from matrix if > 0
+            if graph[u][v]:
+                G.add_edge(u, v, weight=graph[u][v])
+
+
+    nx.draw_networkx(G, pos=pos, labels=name)
+    plt.show()
 
     '''
-    TO DO: Look into NumPy arrays for easier conversion into NX graphs
-    pre-visualization. Also need to figure out basemap points relative to
-    real world coordinates.
+    TO DO: Need to figure out basemap points relative to real world coordinates
     '''
--- mst_build.py
+++ mst_build.py
@@ -1,15 +1,17 @@
 from top48 import *
 
+from graph_visualizer import visualize
+
 def prim(graph):
     n = len(graph)
     included = {0}
     mst = [[0 for x in range(n)] for y in range(n)]
 
     while len(included) < n:
-        minimum = 999999
+        minimum = 999999999
         x, y = 0, 0
         for u in included:
-            for v in range(u + 1, n):
+            for v in range(n):
                 if v not in included and graph[u][v] < minimum:
                     minimum = graph[u][v]
                     x, y = u, v
@@ -17,12 +19,21 @@ def prim(graph):
         mst[x][y] = minimum
         mst[y][x] = minimum
 
-    print(mst)
+    return mst
                 
 
 def main():
 
     mst = prim(g)
+
+    data = []
+
+    for i in range(len(top48)):
+        data.append((top48[i][0], top48[i][5], top48[i][6]))
+
+    print(data)
+
+    visualize(mst, data, 180, 180)
 
 if __name__=="__main__":
     main()
